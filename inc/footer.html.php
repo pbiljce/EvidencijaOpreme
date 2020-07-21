774af0806b8d7422f68b4d5631f23534621af4cf
pbiljce <57287222+pbiljce@users.noreply.github.com>
2020-07-21 16:06:19 +0200

    Add files via upload

---
+++ inc/footer.html.php
@@ -0,0 +1,30 @@
+<footer class="main-footer">
+    <!-- TOP-FOOTER START-->
+    <p class="section-title bottom">Korisnička podrška</p>
+    <div class="top-footer">
+        <div class="item">
+            <p>Korisničko uputstvo</p>
+            <img src="images/pdf.png" alt="Korisničko uputstvo">
+            <a href="#">Uputstvo za korištenje Elektronske evidencije računarske opreme</a>
+        </div>
+        <div class="item">
+            <p>Prijava poteškoća u radu</p>
+            <img src="images/pdf.png" alt="Uputstvo za prijavu">
+            <a href="#">Uputstvo za korisnike za prijavu poteškoća u radu</a>
+        </div>
+        <div class="item">     
+            <p>Kontakt</p>  
+            <img src="images/phone.png" alt="Kontakt telefon">         
+            <span>Nazovite nas: 0123456789</span>
+            <br>
+            <img src="images/email.png" alt="Kontakt email">         
+            <span>eero@gmail.com</span>
+        </div>
+    </div>
+    <!-- TOP-FOOTER END-->
+    <!-- BOTTOM-FOOTER START-->
+    <div class="bottom-footer">
+        <p class="section-title bottom footer">&copy;2020 - Elektronska evidencija računarske opreme</p>
+    </div>
+    <!-- BOTTOM-FOOTER END-->
+</footer>
